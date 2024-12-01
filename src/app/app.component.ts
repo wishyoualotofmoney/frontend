870bcc8dbdb767f2164bc26631229b507a50707e
wishyoualotofmoney <98122726+wishyoualotofmoney@users.noreply.github.com>
2024-12-01 15:53:48 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
@@ -9,8 +10,16 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, HeaderComponent, MenuComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, MenuComponent, FooterComponent, ],
 })
-export class AppComponent {
-  title = 'Барбершоп';
- }
+export class AppComponent implements OnInit {
+  section: string | null = null; // Переменная для хранения текущего раздела
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.section = data['section']; // Получаем данные из маршрута
+    });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent }, // Главная страница
+  { path: 'about', component: AppComponent, data: { section: 'about' } } // Маршрут для "О нашей компании"
+];
